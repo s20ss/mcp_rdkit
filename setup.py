b01991e9438cb7a1e3e8332a4b9ac5464b283106
Shashank Shekhar Shukla <shukla20shashankshekhar@gmail.com>
2025-05-15 05:14:09 +0530

    Added Smithery configuration file

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='mcp_rdkit',
-    version='0.1.4',
+    version='0.1.6',
     author='Shashank Shekhar Shukla',
     author_email='shukla20shashankshekhar@gmail.com',
     description='A package for RDKit integration with MCP',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -17,4 +21,9 @@ setup(
         "rdkit",
         "mcp"
     ],
+    project_urls={
+        "Documentation": "https://github.com/s20ss/mcp_rdkit#readme",
+        "Source": "https://github.com/s20ss/mcp_rdkit",
+        "Issues": "https://github.com/s20ss/mcp_rdkit/issues"
+    },
 )
