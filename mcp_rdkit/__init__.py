d67ccb3592690011ee4ccb25fbd7e93335d396ac
Shashank Shekhar Shukla <shukla20shashankshekhar@gmail.com>
2025-05-18 02:43:43 +0530

    Changing back to STDIO

--- mcp_rdkit/__init__.py
+++ mcp_rdkit/__init__.py
@@ -5,4 +5,4 @@ __version__ = "0.1.0"
 
 
 """Main entry point for the package."""
-mcp.run("streamable-http")
+mcp.run("stdio")
--- mcp_rdkit/__main__.py
+++ mcp_rdkit/__main__.py
@@ -3,4 +3,4 @@ from .rdkit_helper import mcp
 
 __version__ = "0.1.0"
 
-mcp.run("streamable-http")
+mcp.run("stdio")
